bca0cef8bd0dbdd794c752695b61cfc223e2091b
RyanChent <rwqga@qq.com>
2021-05-06 18:11:14 +0800

    'change-something'

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # vitebacken-cli
+
+```bash
+vite-backen-cli init <your-project>
+cd <your-project>
+npm install (or yarn)
+npm run dev (or yarn)
+```
--- bin/vite-backen-cli.js
+++ bin/vite-backen-cli.js
@@ -1,9 +1,6 @@
 #! node
 const program = require('commander')
 const download = require('download-git-repo')
-const inquirer = require('inquirer')
-const fs = require('fs')
-const handlebars = require('handlebars')
 const ora = require('ora')
 const chalk = require('chalk')
 const symbols = require('log-symbols')
@@ -11,43 +8,23 @@ const symbols = require('log-symbols')
 program.version('1.0.0', '-v, --version')
     .command('init <name>')
     .action((name) => {
-        inquirer.prompt([
-            {
-                name: 'description',
-                message: '请输入项目描述'
-            },
-            {
-                name: 'author',
-                message: '请输入作者名称'
+        // 开始下载
+        const spinner = ora('Downloading...');
+        spinner.start();
+        download('github:RyanChent/vite-backen-admin#master', name, { clone: true }, (err) => {
+            if (err) {
+                spinner.fail()
+                console.log(symbols.error, chalk.red('Failed'))
+            } else {
+                spinner.succeed()
             }
-        ]).then((answers) => {
-            // 开始下载
-            const spinner = ora('Downloading...');
-            spinner.start();
-            download('github:RyanChent/vite-backen-admin#master', name, { clone: true }, (err) => {
-                if (err) {
-                    spinner.fail()
-                    console.log(symbols.error, chalk.red('Failed'))
-                } else {
-                    spinner.succeed()
-                }
-                const meta = {
-                    name,
-                    description: answers.description,
-                    author: answers.author
-                }
-                const fileName = `${name}/package.json`
-                const content = fs.readFileSync(fileName).toString()
-                const result = handlebars.compile(content)(meta)
-                fs.writeFileSync(fileName, result)
 
-                console.log(symbols.success, chalk.green('Success'))
-                console.log(`Done. Now run:
+            console.log(symbols.success, chalk.green('Success'))
+            console.log(`Done. Now run:
                 cd ${name}
                 npm install (or yarn)
                 npm run dev (or yarn dev)
                 `)
-            })
         })
     })
 program.parse(process.argv);
